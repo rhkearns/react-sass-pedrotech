38f6094eba3ccc857b92b95872c3f130ae3c9863
rhkearns <ryankearns217@gmail.com>
2022-02-08 17:09:46 -0500

    done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import '../styles/Main.css'
 
 const Main = () => {
   return (
     <div className='main'>
       <h1>Welcome to the World</h1>
+      <button> CLICK ME </button>
     </div>
   )
 }
